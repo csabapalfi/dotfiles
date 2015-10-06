aa0ef9094673802bf0719c9885ae48633a5e1f0f
Csaba Palfi <csaba@palfi.me>
2015-10-06 21:45:46 +0100

    update npm init
    Signed-off-by: Csaba Palfi <csaba@palfi.me>

--- .npm-init.js
+++ .npm-init.js
@@ -1,2 +1,10 @@
 exports.name = basename;
 exports.version = '1.0.0';
+if (!package.description) {
+  exports.description = yes ? '' : prompt('description')
+}
+exports.repository = {
+    type: 'git',
+    url: 'https://github.com/csabapalfi/' + basename + '.git'
+};
+exports.license = 'MIT'
