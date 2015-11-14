7fc9b30ad44676c72aef5c2f0eca98a2e3988982
Csaba Palfi <csaba@palfi.me>
2015-11-14 17:24:50 +0000

    back to super simple npm init
    Signed-off-by: Csaba Palfi <csaba@palfi.me>

--- .npm-init.js
+++ .npm-init.js
@@ -1,10 +1,2 @@
 exports.name = basename;
 exports.version = '1.0.0';
-if (!package.description) {
-  exports.description = yes ? '' : prompt('description')
-}
-exports.repository = {
-    type: 'git',
-    url: 'https://github.com/csabapalfi/' + basename + '.git'
-};
-exports.license = 'MIT'
