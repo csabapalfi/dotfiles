0f1165e4927d7c3539ad36118c3a70428350708b
Csaba Palfi <csaba@palfi.me>
2016-10-05 11:47:54 +0100

    add repo and license to npm init

--- .npm-init.js
+++ .npm-init.js
@@ -1,2 +1,15 @@
-exports.name = basename;
-exports.version = '1.0.0';
+if (!package.name) {
+  exports.name = basename;
+}
+
+if (!package.version) {
+  exports.version = '1.0.0';
+}
+
+if (!package.repository) {
+  exports.repository = `csabapalfi/${basename}`;
+}
+
+if (!package.license) {
+  exports.license = "MIT";
+}
