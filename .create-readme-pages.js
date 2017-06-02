7c4186d94d7040f5caf7fd3b57c0b21dc0b26f57
Csaba Palfi <csaba@palfi.me>
2017-06-02 14:39:42 +0200

    cleanup

--- .create-readme-pages.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  author: {
-    name: 'Csaba Palfi',
-    url: 'https://csabapalfi.github.io'
-  },
-  'github-markdown-html': {
-    gaTrackingId: 'UA-29212656-1'
-  }
-};
--- .vimrc
+++ .vimrc
@@ -1,4 +1,4 @@
-if exists(':pathogen')
-  execute pathogen#infect()
-endif
+set nocompatible
+set backspace=indent,eol,start
 syntax on
+filetype plugin indent on
